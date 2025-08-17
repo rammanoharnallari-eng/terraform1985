2e96ca284dbd861d756d97811e729ace17e61dae
rammanoharnallari-eng <rammanohar.nallari@googlemail.com>
2025-08-17 21:38:51 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,12 +1,14 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.9.0"
     }
   }
+
+  required_version = ">= 1.6.0"
 }
 
 provider "aws" {
- region = "us-east-1"
+  region = "us-east-1"
 }
