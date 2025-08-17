12bea959a2e665ec02fd5dd148f05fe20ad92bca
rammanoharnallari-eng <rammanohar.nallari@googlemail.com>
2025-08-17 21:53:00 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,11 @@
 resource "aws_instance" "worker-node" {
   ami           = "ami-0de716d6197524dd9"
   instance_type = "t2.micro"
-  tenancy       = "host"
+  #tenancy       = "host"
 
   tags = {
     Name        = "worker-node"
     Environment = "Development"
   }
 }
+
